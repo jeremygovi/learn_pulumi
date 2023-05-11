b5ef3cdf8180ae71389acdea4c8afeece696b204
jeremygovi <jeremygovi@gmail.com>
2023-05-11 13:24:53 +0200

    put a real name to the dest bucket

--- __main__.py
+++ __main__.py
@@ -4,7 +4,7 @@ import pulumi
 from pulumi_aws import s3
 
 # Create an AWS resource (S3 Bucket)
-bucket = s3.Bucket('my-bucket')
+bucket = s3.Bucket('poc-gbfs-payload', bucket='poc-gbfs-payload')
 
 # Export the name of the bucket
 pulumi.export('bucket_name', bucket.id)
