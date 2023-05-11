a20fa15c0b2db066aa5591bc7aaa6617d5debc2d
jeremygovi <jeremygovi@gmail.com>
2023-05-11 13:52:27 +0200

    DO NOT MERGE: rename the bucket

--- __main__.py
+++ __main__.py
@@ -4,7 +4,7 @@ import pulumi
 from pulumi_aws import s3
 
 # Create an AWS resource (S3 Bucket)
-bucket = s3.Bucket('poc-gbfs-payload', bucket='poc-gbfs-payload')
+bucket = s3.Bucket('xxx', bucket='xxx')
 
 # Export the name of the bucket
 pulumi.export('bucket_name', bucket.id)
