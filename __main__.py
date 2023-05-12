91639da8f8bc33ff7038589f3331cca177a8b359
jeremygovi <jeremygovi@gmail.com>
2023-05-12 13:51:45 +0200

    Full working version end to end + tune scalability/perf (1m30 to 32seconds)

--- __main__.py
+++ __main__.py
@@ -1,6 +1,14 @@
 import pulumi
 import pulumi_aws as aws
 
+# Vars definition
+event_target_name = "gbfs-cron-lambda-target"
+cron_trigger_enabled = False
+lambda_sqs_to_s3_batch_size = 1
+lambda_execution_timeout = 60
+sqs_messages_retention = 1209600  # the max value (need resilience)
+alarm_enabled = False
+
 ##################
 ## S3 Bucket
 ##################
@@ -14,6 +22,17 @@ bucket = aws.s3.Bucket(
 )
 # Export the name of the bucket
 pulumi.export("bucket_name", bucket.bucket)
+
+
+######################################################
+## SQS Queue (for scaling / higer performances)
+######################################################
+queue = aws.sqs.Queue(
+    "gbfs-queue",
+    message_retention_seconds=sqs_messages_retention,
+    visibility_timeout_seconds=lambda_execution_timeout,
+)
+pulumi.export("queue_name", queue.name)
 
 
 ##################
@@ -36,11 +55,12 @@ lambda_role = aws.iam.Role(
     }""",
 )
 
+arns = pulumi.Output.all(bucket.arn, queue.arn)
 lambda_role_policy = aws.iam.RolePolicy(
     "gbfsLambdaRolePolicy",
     role=lambda_role.id,
-    policy=pulumi.Output.all(bucket.arn).apply(
-        lambda arn: f"""{{
+    policy=arns.apply(
+        lambda arns: f"""{{
             "Version": "2012-10-17",
             "Statement": [
                 {{
@@ -56,15 +76,35 @@ lambda_role_policy = aws.iam.RolePolicy(
                     "Effect": "Allow",
                     "Action": "s3:*",
                     "Resource": [
-                        "{arn[0]}/*",
-                        "{arn[0]}"
+                        "{arns[0]}/*",
+                        "{arns[0]}"
                     ]
+                }},
+                {{
+                    "Effect": "Allow",
+                    "Action": [
+                        "sqs:SendMessage",
+                        "sqs:ReceiveMessage",
+                        "sqs:DeleteMessage",
+                        "sqs:GetQueueAttributes"
+                    ],
+                    "Resource": "{arns[1]}"
                 }}
             ]
         }}"""
     ),
 )
 
+
+####################################
+## Event rule (cloudwatch part)
+####################################
+
+event_rule = aws.cloudwatch.EventRule(
+    "trigger-every-minute-rule",
+    schedule_expression="rate(1 minute)",
+    is_enabled=cron_trigger_enabled,
+)
 
 ##################
 ## Lambda Function
@@ -76,25 +116,29 @@ lambda_func = aws.lambda_.Function(
     runtime="python3.8",
     handler="gbfs_parser.main",
     code=pulumi.AssetArchive({".": pulumi.FileArchive("./lambda")}),
+    environment={
+        "variables": {
+            "BUCKET_NAME": bucket.id,
+            "QUEUE_URL": queue.url,
+            "QUEUE_ARN": queue.arn,
+            "EVENT_RULE_ARN": event_rule.arn,
+        }
+    },
+    timeout=lambda_execution_timeout,
 )
 
 
 ####################################
-## Lambda trigger (cloudwatch part)
+## Event Target (cloudwatch part)
 ####################################
-
-event_rule = aws.cloudwatch.EventRule(
-    "trigger-every-minute-rule",
-    schedule_expression="rate(1 minute)",
-    is_enabled=False,
-)
 
 event_target = aws.cloudwatch.EventTarget(
-    "gbfs-lambda-target",
+    event_target_name,
     rule=event_rule.name,
     arn=lambda_func.arn,
 )
 
+# Allow cloudwatch to call lambda
 cloudwatch_to_lambda_permission = aws.lambda_.Permission(
     "gbfs-lambda-cloudwatch-permission",
     action="lambda:InvokeFunction",
@@ -127,7 +171,18 @@ alarm = aws.cloudwatch.MetricAlarm(
     period=60,
     statistic="Sum",
     threshold=0,
-    actions_enabled=True,
+    actions_enabled=alarm_enabled,
     alarm_actions=[alarm_topic.arn],
     dimensions={"FunctionName": lambda_func.name},
+)
+
+############################
+## EventSourceMapping
+############################
+# Trigger the lambda function when there are messages in the SQS queue
+lambda_event_source_mapping = aws.lambda_.EventSourceMapping(
+    "sqs-to-lambda-event-source-mapping",
+    event_source_arn=queue.arn,
+    function_name=lambda_func.arn,
+    batch_size=lambda_sqs_to_s3_batch_size,
 )
--- lambda/gbfs_parser.py
+++ lambda/gbfs_parser.py
@@ -1,5 +1,96 @@
 import json
+import logging
+import os
+import boto3
+import urllib.request
+from datetime import datetime
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+timestamp = datetime.utcnow().strftime("%Y%m%d_%H:%M:%S")
+QUEUE_URL = os.getenv("QUEUE_URL")
+QUEUE_ARN = os.getenv("QUEUE_ARN")
+BUCKET_NAME = os.getenv("BUCKET_NAME")
+EVENT_RULE_ARN = os.getenv("EVENT_RULE_ARN")
+
+sqs = boto3.client("sqs")
+s3 = boto3.resource("s3")
+
+
+def add_metadata(bike):
+    bike["timestamp"] = timestamp
+    bike["tags"] = {
+        "is_disabled": bike["is_disabled"],
+        "is_reserved": bike["is_reserved"],
+        "timestamp": timestamp,
+    }
+    return bike
+
+
+def parse_url_and_push_to_sqs():
+    url = "https://mds.bird.co/gbfs/tempe/free_bike_status.json"
+    logger.info(f"Getting bikes from url {url}")
+    res = urllib.request.urlopen(
+        urllib.request.Request(
+            url=url,
+            headers={"Accept": "application/json"},
+            method="GET",
+        ),
+        timeout=10,
+    )
+    if res.status == 200:
+        data = json.loads(res.read())["data"]
+        bikes = data["bikes"]
+        logger.info(f"Processing {len(bikes)} bikes")
+        for bike in bikes:
+            bike = add_metadata(bike)
+            message_body = {"bike": bike}
+            response = sqs.send_message(
+                QueueUrl=QUEUE_URL, MessageBody=json.dumps(message_body)
+            )
+            logger.info(f"Message {response['MessageId']} sent to SQS queue")
+            # # TO REMOVE
+            # exit(0)
+
+    else:
+        logger.error(f"Error fetching data from {url}, status code {res.status}")
+
+
+def get_from_sqs_and_push_to_s3(data):
+    bike = json.loads(data)["bike"]
+    bike_id = bike["bike_id"]
+    bike = add_metadata(bike)
+    file_name = f"{bike_id}.json"
+    file_content = json.dumps(bike)
+    object = s3.Object(BUCKET_NAME, file_name)
+    object.put(
+        Body=file_content,
+        Tagging=f"timestamp={timestamp}&is_disabled={bike['is_disabled']}&is_reserved={bike['is_reserved']}",
+    )
+    logger.info(f"S3 object {file_name} saved")
 
 
 def main(event, context):
-    return {"statusCode": 200, "body": json.dumps("Cheers from AWS Lambda!!")}
+    # logger.debug(f"DEBUG event: {event}")
+    if (
+        "source" in event
+        and event["source"] == "aws.events"
+        and "detail-type" in event
+        and event["detail-type"] == "Scheduled Event"
+        and "resources" in event
+        and event["resources"][0] == EVENT_RULE_ARN
+    ):
+        logger.info(f"Lambda triggered by {EVENT_RULE_ARN} cron")
+        parse_url_and_push_to_sqs()
+    elif (
+        "Records" in event
+        and len(event["Records"]) > 0
+        and "eventSourceARN" in event["Records"][0]
+        and event["Records"][0]["eventSourceARN"] == QUEUE_ARN
+    ):
+        logger.info(f"Lambda triggered by SQS message event")
+        get_from_sqs_and_push_to_s3(event["Records"][0]["body"])
+    else:
+        logger.error("Unknown event source")
+        exit(1)
